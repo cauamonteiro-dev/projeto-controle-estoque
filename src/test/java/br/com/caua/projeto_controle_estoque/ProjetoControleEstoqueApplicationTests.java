912811c65f637f6119a35e28ef1d618652fbd319
Cauã Monteiro <caua.developer07@gmail.com>
2025-12-16 14:37:37 -0300

    Ajuste da estrutura do projeto e arquivos de configuração git

---
+++ src/test/java/br/com/caua/projeto_controle_estoque/ProjetoControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caua.projeto_controle_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
