74ff1d64b9ea7539d9424fb4f69265c1368287e9
Cauã Monteiro <caua.developer07@gmail.com>
2025-12-16 14:37:37 -0300

    Ajuste da estrutura do projeto e arquivos de configuração git

---
+++ src/main/java/br/com/caua/projeto_controle_estoque/ProjetoControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.caua.projeto_controle_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleEstoqueApplication.class, args);
+	}
+
+}
