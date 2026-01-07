238d81ad5822955d500f418996fd29e47d937d18
Cauã Monteiro <caua.developer07@gmail.com>
2026-01-07 11:03:31 -0300

    Criação do Controller e Service
    Também ajustado o controller para alinhar com service e tratar exceções

---
+++ src/main/java/br/com/caua/projeto_controle_estoque/controller/ProdutoController.java
@@ -0,0 +1,51 @@
+package br.com.caua.projeto_controle_estoque.controller;
+
+import br.com.caua.projeto_controle_estoque.entity.Produto;
+import br.com.caua.projeto_controle_estoque.service.ProdutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public Produto criarProduto(@RequestBody Produto produto) {
+        return produtoService.salvar(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
+        try {
+            Produto produto = produtoService.buscarPorId(id);
+            return ResponseEntity.ok(produto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        try {
+            produtoService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
---
+++ src/main/java/br/com/caua/projeto_controle_estoque/service/ProdutoService.java
@@ -0,0 +1,41 @@
+package br.com.caua.projeto_controle_estoque.service;
+
+import br.com.caua.projeto_controle_estoque.entity.Produto;
+import br.com.caua.projeto_controle_estoque.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> listarTodos() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto buscarPorId(Long id) {
+        return produtoRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Produto não encontrado com ID: " + id)
+                );
+    }
+
+    public void deletar(Long id) {
+        if (!produtoRepository.existsById(id)) {
+            throw new RuntimeException("Produto não encontrado com ID: " + id);
+        }
+        produtoRepository.deleteById(id);
+    }
+
+
+}
